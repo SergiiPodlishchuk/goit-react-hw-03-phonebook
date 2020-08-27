6ee35eedce0fcac98974cde8547f58b3a998a3a0
SergiiPodlishchuk <podluy23@gmail.com>
2020-08-28 00:39:47 +0300

    Updates

---
+++ precache-manifest.f76f19bf994fd998e8d0f7e0852e722c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6389b26160bc00bc1ba207044c5953bc",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "fede1cc890cd26183325",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.d75e25b6.chunk.css"
+  },
+  {
+    "revision": "b73a71fb937eb9792ce1",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.9ac5478f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.9ac5478f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fede1cc890cd26183325",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.cc2a8cb9.chunk.js"
+  },
+  {
+    "revision": "159f9e7c304b75423aa5",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.cca09c81.js"
+  }
+]);
